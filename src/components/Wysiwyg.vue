23eb2cbe98dddaa94f2c0cbd5e46ad63c9610f3d
Gwalchmei <darainas2@gmail.com>
2020-07-08 17:33:44 +0200

    toggle style for selected text and delete text

--- src/components/Wysiwyg.vue
+++ src/components/Wysiwyg.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <div class="mb-2">
+      <button :class="{ 'active': currentStyle === 'span'}" @click="toggleStyle('span')">Normal</button>
       <button :class="{ 'active': currentStyle === 'b' }" @click="toggleStyle('b')">Bold</button>
       <button :class="{ 'active': currentStyle === 'i' }" @click="toggleStyle('i')">Italic</button>
     </div>
-    <div ref="container">
+    <div ref="container" tabindex="0" @keydown.backspace="deleteText" class="wysiwyg-container" style="{ wordBreak: 'break-word' }">
       <component v-for="(item, index) in styles" :is="item.type" :key="index">{{ item.value }}</component>
       <input ref="input" :value="''" :style="inputStyle" @input="handleInput"/>
     </div>
@@ -32,18 +33,25 @@ export default {
       return {}
     }
   },
+  mounted: function () {
+    this.$refs.input.focus()
+  },
   methods: {
-    handleInput: function (e) {
-      const style = this.styles.pop()
-      style.value += e.target.value
-      this.styles.push(style)
-    },
-    toggleStyle: function (style) {
+    hasSelectedText: function () {
       const selection = window.getSelection()
-      if (selection && !selection.isCollapsed && this.$refs.container.contains(selection.anchorNode) && this.$refs.container.contains(selection.focusNode)) {
+      return selection &&
+        !selection.isCollapsed &&
+        this.$refs.container.contains(selection.anchorNode) &&
+        this.$refs.container.contains(selection.focusNode)
+    },
+    getSelectionRange: function () {
+      if (this.hasSelectedText()) {
+        const selection = window.getSelection()
         const nodes = this.$refs.container.childNodes
         const anchorNode = selection.anchorNode.nodeType === Node.TEXT_NODE ? selection.anchorNode.parentNode : selection.anchorNode
         const focusNode = selection.focusNode.nodeType === Node.TEXT_NODE ? selection.focusNode.parentNode : selection.focusNode
+        let anchorOffset = selection.anchorOffset
+        let focusOffset = selection.focusOffset
         let startIndex = 0
         let endIndex = 0
         for (let i = 0; i < nodes.length; i += 1) {
@@ -54,22 +62,125 @@ export default {
             endIndex = i
           }
         }
-        const precedingStyles = this.styles.filter((_, i) => i < startIndex)
-        const nextStyles = this.styles.filter((_, i) => i > endIndex)
-        const startStyle = this.styles[startIndex]
-        if (selection.anchorOffset > 0) {
-          precedingStyles.push({ type: startStyle.type, value: startStyle.value.substring(0, selection.anchorOffset) })
+        if (startIndex > endIndex) {
+          const tempIndex = startIndex
+          const tempOffset = anchorOffset
+          startIndex = endIndex
+          anchorOffset = focusOffset
+          endIndex = tempIndex
+          focusOffset = tempOffset
         }
-        let newValue = startStyle.value.substring(selection.anchorOffset)
-        newValue += this.styles.filter((_, i) => i > startIndex && i < endIndex).map(({ value }) => value).join()
-        const endStyle = this.styles[endIndex]
-        if (selection.focusOffset > 0) {
-          console.log(selection.focusOffset, endStyle.value.length)
-          nextStyles.unshift({ type: endStyle.type, value: endStyle.value.substring(selection.focusOffset) })
+        if (startIndex === endIndex && anchorOffset > focusOffset) {
+          const tempOffset = anchorOffset
+          anchorOffset = focusOffset
+          focusOffset = tempOffset
         }
-        newValue += endStyle.value.substring(0, selection.focusOffset)
-        this.styles = [...precedingStyles, { type: style, value: newValue }, ...nextStyles]
+
         selection.empty()
+
+        return {
+          from: startIndex,
+          fromOffset: anchorOffset,
+          to: endIndex,
+          toOffset: focusOffset
+        }
+      }
+
+      return null
+    },
+    setStyles: function (styles) {
+      this.styles = styles.reduce((clean, style) => {
+        if (clean.length > 0) {
+          const last = clean.pop()
+          if (last.type === style.type) {
+            return [...clean, { type: last.type, value: last.value + style.value }]
+          }
+
+          return [...clean, last, style]
+        }
+
+        return [clean, style]
+      }, [])
+    },
+    deleteText: function () {
+      const selectionRange = this.getSelectionRange()
+      if (selectionRange) {
+        const precedingStyles = this.styles.filter((_, i) => i < selectionRange.from)
+        const nextStyles = this.styles.filter((_, i) => i > selectionRange.to)
+        const startStyle = this.styles[selectionRange.from]
+        if (selectionRange.fromOffset > 0) {
+          precedingStyles.push({ type: startStyle.type, value: startStyle.value.substring(0, selectionRange.fromOffset) })
+        }
+        if (selectionRange.from === selectionRange.to) {
+          if (selectionRange.toOffset > 0) {
+            nextStyles.unshift({ type: startStyle.type, value: startStyle.value.substring(selectionRange.toOffset) })
+          }
+          this.setStyles([
+            ...precedingStyles,
+            ...nextStyles
+          ])
+        } else {
+          const endStyle = this.styles[selectionRange.to]
+          if (selectionRange.toOffset > 0) {
+            nextStyles.unshift({ type: endStyle.type, value: endStyle.value.substring(selectionRange.toOffset) })
+          }
+          this.setStyles([...precedingStyles, ...nextStyles])
+        }
+      } else {
+        if (this.styles.length > 0) {
+          const style = this.styles.pop()
+          style.value = style.value.slice(0, -1)
+          if (style.value.length > 0) {
+            this.styles.push(style)
+          }
+        }
+      }
+    },
+    handleInput: function (e) {
+      if (this.styles.length > 0) {
+        const style = this.styles.pop()
+        if (style.type === this.currentStyle) {
+          style.value += e.target.value
+          this.styles.push(style)
+        } else {
+          this.styles.push(style)
+          this.styles.push({ type: this.currentStyle, value: e.target.value })
+        }
+      } else {
+        this.styles.push({ type: this.currentStyle, value: e.target.value })
+      }
+    },
+    toggleStyle: function (style) {
+      const selectionRange = this.getSelectionRange()
+      if (selectionRange) {
+        const precedingStyles = this.styles.filter((_, i) => i < selectionRange.from)
+        const nextStyles = this.styles.filter((_, i) => i > selectionRange.to)
+        const startStyle = this.styles[selectionRange.from]
+        if (selectionRange.fromOffset > 0) {
+          precedingStyles.push({ type: startStyle.type, value: startStyle.value.substring(0, selectionRange.fromOffset) })
+        }
+        if (selectionRange.from === selectionRange.to) {
+          if (selectionRange.toOffset > 0) {
+            nextStyles.unshift({ type: startStyle.type, value: startStyle.value.substring(selectionRange.toOffset) })
+          }
+          this.setStyles([
+            ...precedingStyles,
+            { type: style, value: startStyle.value.substring(selectionRange.fromOffset, selectionRange.toOffset) },
+            ...nextStyles
+          ])
+        } else {
+          let newValue = startStyle.value.substring(selectionRange.fromOffset)
+          newValue += this.styles
+            .filter((_, i) => i > selectionRange.from && i < selectionRange.to)
+            .map(({ value }) => value)
+            .join('')
+          const endStyle = this.styles[selectionRange.to]
+          if (selectionRange.toOffset > 0) {
+            nextStyles.unshift({ type: endStyle.type, value: endStyle.value.substring(selectionRange.toOffset) })
+          }
+          newValue += endStyle.value.substring(0, selectionRange.toOffset)
+          this.setStyles([...precedingStyles, { type: style, value: newValue }, ...nextStyles])
+        }
       } else {
         this.currentStyle = this.currentStyle === style ? 'span' : style
         this.styles.push({ type: this.currentStyle, value: '' })
@@ -83,6 +194,14 @@ export default {
 <style lang="scss">
 input {
   border: none;
+
+  &:focus {
+    outline: none;
+  }
+}
+
+.wysiwyg-container {
+  word-break: break-word;
 
   &:focus {
     outline: none;
